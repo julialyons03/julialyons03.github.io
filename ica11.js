f1a85a77684d702d0488ffa80aeaeacc9639a819
julialyons03 <july1454@colorado.edu>
2023-04-04 10:28:45 -0600

    changes

---
+++ ica11.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("#js-new-quote");
+button.addEventListener('click', getQuote);
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function getQuote(){
+    //console.log("It works!");
+    try{
+        const response = await fetch(endpoint)
+        if(!response.ok){
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+        displayQuote (json.question);
+        //console.log(json.question);
+    }
+    catch (err) {
+        console.log(err);
+        alert('Failed to fetch new trivia');
+    }
+}
+
+function displayQuote(quote){
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+getQuote();
+
+
+
