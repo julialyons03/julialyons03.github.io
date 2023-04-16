7b782f7d9ae5a137e4c922516d8d02b3bc06c78b
julialyons03 <july1454@colorado.edu>
2023-04-16 14:50:33 -0600

    wa12

--- wa12.js
+++ wa12.js
@@ -7,98 +7,92 @@
 // Problem 1:
 // create JSON for employees
 
-const employeesList = [
-    {
-        firstName: 'Sam',
-        department: 'Tech',
-        designation: 'Manager',
-        salary: 40000,
-        raiseEligible: true
-        },
-        {
-        firstName: 'Mary',
-        department: 'Finance',
-        designation: 'Trainee',
-        salary: 18500,
-        raiseEligible: true
-        },
-        {
-        firstName: 'Bill',
-        department: 'HR',
-        designation: 'Executive',
-        salary: 21200,
-        raiseEligible: false
-        }
-        ];
-
-
-// Problem 2:
-// create JSON for company
-
-const company = {
-    companyName: 'Tech Stars',
-    website: 'www.techstars.site',
-    employees: employeesList
+const sam = {
+    firstName: "Sam",
+    department: "Tech",
+    designation: "Manager",
+    salary: 40000,
+    raiseEligible: true,
     };
+    
+    const mary = {
+    firstName: "Mary",
+    department: "Finance",
+    designation: "Trainee",
+    salary: 18500,
+    raiseEligible: true,
+    };
+    
+    const bill = {
+    firstName: "Bill",
+    department: "HR",
+    designation: "Executive",
+    salary: 21200,
+    raiseEligible: false,
+    };
+    
+    console.log(sam);
+    console.log(mary);
+    console.log(bill);
+    
+    // Problem 2
+    // create JSON for company
+
+    const employeeList = [sam, mary, bill];
+    
+    const techStars = {
+    companyName: "Tech Stars",
+    website: "www.techstars.site",
+    employees: employeeList,
+    };
+    
+    console.log(techStars);
+    
+    // Problem 3
+    // add anna
 
     const anna = {
-        firstName: 'Anna',
-        department: 'Tech',
-        designation: 'Executive',
-        salary: 25600,
-        raiseEligible: false
-        };
-
-        if (company.employeesList) {
-            company.employeesList.push(anna);
-          } else {
-            company.employeesList = [anna];
-          }
-        console.log(company);
+    firstName: "Anna",
+    department: "Tech",
+    designation: "Executive",
+    salary: 25600,
+    raiseEligible: false,
+    };
     
-// Problem 3: (see lines 44-57)
-// add anna
+    techStars.employees.push(anna);
+    
+    console.log(techStars);
+    
+    // Problem 4:
+    // calculate total salary
 
-// Problem 4:
-// calculate total salary
-
-let totalSalary = 0;
-for (let i = 0; i < company.employeesList.length; i++) {
-totalSalary += company.employeesList[i].salary;
-}
-console.log(totalSalary);
-
-// Problem 5:
-// give raise of 10% or set eligibility to false
-
-function raise(employeesList) {
-    for (let i = 0; i < employeesList.length; i++) {
-    if (employeesList[i].raiseEligible) {
-    employeesList[i].salary *= 1.1;
-    employeesList[i].raiseEligible = false;
+    let totalSalary = 0;
+    for (let i = 0; i < techStars.employees.length; i++) {
+    totalSalary += techStars.employees[i].salary;
     }
+    
+    console.log(totalSalary);
+    
+    // Problem 5
+    // give raise of 10% or set eligibility to false
+
+    function raise(employee) {
+    if (employee.raiseEligible) {
+    employee.salary *= 1.1;
+    employee.raiseEligible = false;
     }
     }
     
-    raise(company.employeesList);
-    console.log(company);
-
-//Problem 6:
-// update company JSON for people working from home
-
-const workFromHome = ['Anna', 'Sam'];
-for (let i = 0; i < company.employeesList.length; i++) {
-if (workFromHome.includes(company.employeesList[i].firstName)) {
-company.employeesList[i].wfh = true;
-} else {
-company.employeesList[i].wfh = false;
-}
-}
-
-console.log(company);
-
-
-
-
-
-
+    techStars.employees.forEach(raise);
+    console.log(techStars);
+    
+    // Problem 6
+    // update company JSON for people working from home
+    
+    const workFromHome = ["Anna", "Sam"];
+    
+    techStars.employees.forEach((employee) => {
+    employee.wfh = workFromHome.includes(employee.firstName);
+    });
+    
+    console.log(techStars);
